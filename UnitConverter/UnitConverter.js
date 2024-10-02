00e9dc81db011a1755007e6415eb7129a635bd6c
Jakob Rejdemyhr <jakob.rejdemyhr@gmail.com>
2024-10-02 15:16:25 +0200

    add weightConverter

--- UnitConverter/UnitConverter.js
+++ UnitConverter/UnitConverter.js
@@ -97,6 +97,14 @@ export class UnitConverter {
     }
   }
 
+  /**
+   * Converts a number of minutes to a specified unit of time.
+   *
+   * @param {number} number - The number of units to convert.
+   * @param {string} fromUnit - The unit type to convert from. Can be 'celsius', 'fahrenheit', or 'kelvin'.
+   * @param {string} toUnit - The unit to convert to. Can be 'celsius', 'fahrenheit', or 'kelvin'.
+   * @returns {number | boolean} - The converted value, or false if the input is invalid.
+   */
   tempConverter (number, fromUnit, toUnit) {
     if (!this.#validateNumber(number)) {
       return 'invalidInput'
@@ -151,6 +159,85 @@ export class UnitConverter {
   }
 
   /**
+   * 
+   * @param {number} number - The number of units to convert.
+   * @param {string} fromUnit - The unit type to convert from. Can be 'grams', 'kilograms', 'ounces' or 'pounds'.
+   * @param {*} toUnit 
+   * @returns 
+   */
+  weightConverter (number, fromUnit, toUnit) {
+    if (!this.#validateNumber(number)) {
+      return 'invalidInput'
+    }
+
+    if (!this.#validateWeightUnitType(toUnit)) {
+      return 'invalidInput'
+    }
+
+    if (!this.#validateWeightUnitType(fromUnit)) {
+      return 'invalidInput'
+    }
+
+    switch (toUnit) {
+      case 'grams':
+        return this.#convertToGrams(fromUnit, number)
+      case 'kilograms':
+        return this.#convertToKilograms(fromUnit, number)
+      case 'pounds':
+        return this.#convertToPounds(fromUnit, number)
+      case 'ounces':
+        return this.#convertToOunces(fromUnit, number)
+    }
+  }
+
+  #convertToGrams (fromUnit, number) {
+    if (fromUnit === 'kilograms') {
+      return number * 1000
+    } else if (fromUnit === 'pounds') {
+      return number * 453.592
+    } else if (fromUnit === 'ounces') {
+      return number * 28.3495
+    } else {
+      return number
+    }
+  }
+
+  #convertToKilograms (fromUnit, number) {
+    if (fromUnit === 'grams') {
+      return number / 1000
+    } else if (fromUnit === 'pounds') {
+      return number / 2.205
+    } else if (fromUnit === 'ounces') {
+      return number / 35.274
+    } else {
+      return number
+    }
+  }
+
+  #convertToPounds (fromUnit, number) {
+    if (fromUnit === 'grams') {
+      return number / 453.592
+    } else if (fromUnit === 'kilograms') {
+      return number * 2.205
+    } else if (fromUnit === 'ounces') {
+      return number / 16
+    } else {
+      return number
+    }
+  }
+
+  #convertToOunces (fromUnit, number) {
+    if (fromUnit === 'grams') {
+      return number / 28.3495
+    } else if (fromUnit === 'kilograms') {
+      return number * 35.274
+    } else if (fromUnit === 'pounds') {
+      return number * 16
+    } else {
+      return number
+  }
+
+  /**
    * Converts a number of minutes to a specified unit of time.
    *
    * @param {number} number - The number of minutes to convert
@@ -188,6 +275,19 @@ export class UnitConverter {
       unit === 'celsius' ||
       unit === 'fahrenheit' ||
       unit === 'kelvin'
+    ) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  #validateWeightUnitType (unit) {
+    if (
+      unit === 'grams' ||
+      unit === 'kilograms' ||
+      unit === 'pounds' ||
+      unit === 'ounces'
     ) {
       return true
     } else {
